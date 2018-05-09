c960106c440d0259e27e021068c1c5f5f0572214
Brian Cooper <brian@allegro.com>
2018-05-08 20:30:26 -0500

    es2015 and helloworld

--- routes/app.js
+++ routes/app.js
@@ -1,1 +1,12 @@
-// TODO: main logic
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+// TODO: put port in env variable
+app.listen(3000, () => {
+	console.log('queue application server listening on port 3000')
+});
