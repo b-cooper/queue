b62dfce48144b6b78047560a43bba8f4d5458ba3
Brian Cooper <brian@allegro.com>
2018-05-09 14:15:24 -0500

    api frame

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ import uuid from 'uuid';
 import moment from 'moment';
 import _ from 'lodash';
 
-
 const app = express();
 
-const message_id_queue = [];
+
 
 const createMessage = ({text}) => ({
 	id: uuid(),
@@ -16,9 +15,28 @@ const createMessage = ({text}) => ({
 	assigned_at: null
 });
 
+/**************************************
+* Queue Abstraction
+***************************************/
 
-// NOTE: this is object is a stub for a real DB
+const message_id_queue = [];
+const enqueueMessageId = (message_id) => {
+	message_id_queue = [...message_id_queue, message_id]
+};
+const dequeueMessageId = () => {
+	next_id = message_id_queue[0];
+	message_id_queue = message_id_queue.slice(1)
+	return next_id;
+};
+
+/**************************************
+* Database Abstraction
+***************************************/
+
+// this is object will act as our DB
+// a map keyed by, with message data/metadata value
 const message_db = {};
+
 const addMessage = (message) => {
 	message_db = {...message_db, [message.id]: message}
 };
@@ -27,9 +45,33 @@ const deleteMessage = (message_id) => {
 };
 
 
+/**************************************
+ * API endpoints
+ **************************************/
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+// producer adds message (Create)
+app.post('/message', function (req, res) {
+	console.log(req);
+	// TODO: validate message
+	// TODO: return 200 and message_id
+  res.send('POST request to add message')
+});
+
+// consumer requests messages to process (Read)
+app.get('/message', function (req, res) {
+	console.log(req);
+	// TODO: call into staleness dequeue logic and return message
+  res.send('GET request to retrieve messages')
+});
+
+// consumer processes message (Delete)
+app.delete('/message', function (req, res) {
+	console.log(req);
+  res.send('DELETE request to process message')
 });
 
 // TODO: put port in env variable
