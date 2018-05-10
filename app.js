157039bbc08d25368793ee5248d212f5b764c0ea
Brian Cooper <brian@allegro.com>
2018-05-09 20:48:22 -0500

    add, log, retrieve

--- app.js
+++ app.js
@@ -13,18 +13,50 @@ const createMessage = ({text}) => ({
 	assigned_at: null
 });
 
+
 /**************************************
 * Queue Abstraction
 ***************************************/
 
+// let unassigned_message_ids = [];
+// let assigned_message_ids = [];
 let message_id_queue = [];
-const enqueueMessageId = (message_id) => {
-	message_id_queue = [...message_id_queue, message_id]
+
+const enqueueMessageIds = (...message_ids) => {
+	message_id_queue = [...message_id_queue, ...message_ids]
 };
-const dequeueMessageId = () => {
-	next_id = message_id_queue[0];
-	message_id_queue = message_id_queue.slice(1)
-	return next_id;
+
+const dequeueStaleMessageIds = () => {
+	let has_checked_for_stale_ids = false;
+	let current_index = 0;
+	while (!has_checked_for_stale_ids){
+		const current_id = message_id_queue[current_index];
+		if (current_id && isMessageStale(current_id)){
+			current_index = current_index + 1;
+		} else {
+			has_checked_for_stale_ids = true;
+		}
+	}
+	const stale_ids = message_id_queue.slice(0, current_index);
+	stale_ids.forEach(markAsUnassigned);
+	message_id_queue = message_id_queue.slice(current_index);
+	return stale_ids;
+};
+
+const allUnassigned = () => {
+	let has_checked_for_unassigned_ids = false;
+	let current_index = message_id_queue.length - 1;
+	while (!has_checked_for_unassigned_ids){
+		const current_id = message_id_queue[current_index];
+		console.log('in all unassigned', current_id, current_id && isMessageUnassigned(current_id));
+		if (current_id && isMessageUnassigned(current_id)){
+			current_index = current_index - 1;
+		} else {
+			has_checked_for_unassigned_ids = true;
+		}
+	}
+	const unassigned_ids = message_id_queue.slice(current_index + 1)
+	return unassigned_ids;
 };
 
 /**************************************
@@ -36,7 +68,22 @@ const dequeueMessageId = () => {
 let message_db = {};
 
 const addMessage = (message) => {
+	console.log(message);
 	message_db = {...message_db, [message.id]: message}
+}
+const markAsAssigned = (message_id) => {
+	message_db[message_id].assigned_at = moment();
+};;
+const markAsUnassigned = (message_id) => {
+	message_db[message_id].assigned_at = null;
+};
+const isMessageUnassigned = (message_id) => (
+	_.isEmpty(message_db[message_id].assigned_at)
+);
+const isMessageStale = (message_id) => {
+	const message = message_db[message_id];
+	// TODO: use moment helpers
+	return message.assigned_at && ((moment() - message.assigned_at) < process.env.time_limit)
 };
 const deleteMessage = (message_id) => {
 	message_db = _.omit(message_id);
@@ -52,24 +99,42 @@ app.get('/', (req, res) => {
 });
 
 // producer adds message (Create)
-app.post('/message', function (req, res) {
-	console.log(req);
-	// TODO: validate message
-	// TODO: return 200 and message_id
-  res.send('POST request to add message')
+app.post('/message', (req, res) => {
+	// TODO: use POST body
+	// message_text = _.get(req, 'body.name');
+	const message_text = _.get(req, 'query.text');
+	if (message_text) {
+		const message = createMessage({text: message_text});
+		addMessage(message);
+		enqueueMessageIds(message.id)
+		res.send(200, `Your message was registered with id: ${message.id}`);
+	} else {
+		res.send(404, 'Message creation failed, try adding text');
+	}
 });
 
 // consumer requests messages to process (Read)
-app.get('/message', function (req, res) {
+app.get('/message', (req, res) => {
 	console.log(req);
 	// TODO: call into staleness dequeue logic and return message
-  res.send('GET request to retrieve messages')
+	const stale_ids = dequeueStaleMessageIds();
+	enqueueMessageIds(...stale_ids)
+	const next_assigned_ids = allUnassigned();
+	next_assigned_ids.forEach(markAsAssigned);
+	res.send(_.map(next_assigned_ids, id => (message_db[id])));
 });
 
 // consumer processes message (Delete)
-app.delete('/message', function (req, res) {
+app.delete('/message', (req, res) => {
 	console.log(req);
   res.send('DELETE request to process message')
+});
+
+// debug endpoint
+app.get('/state', (req, res) => {
+	console.log('queue', message_id_queue);
+	console.log('db', message_db);
+	res.send(_.map(message_id_queue, id => (message_db[id])))
 });
 
 // TODO: put port in env variable
