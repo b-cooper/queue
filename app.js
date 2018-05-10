bf1dc087b890432dd45d8e7d576c0914a8b0966c
Brian Cooper <brian@allegro.com>
2018-05-09 21:36:44 -0500

    clean and annotate

--- app.js
+++ app.js
@@ -11,14 +11,18 @@ const app = express();
 * Queue Abstraction
 ***************************************/
 
-// let unassigned_message_ids = [];
-// let assigned_message_ids = [];
 let message_id_queue = [];
 
-const enqueueMessageIds = (...message_ids) => {
-	message_id_queue = [...message_id_queue, ...message_ids]
-};
+// ** Queue Mutators **
 
+const enqueueMessageIds = (...message_ids) => { message_id_queue = [...message_id_queue, ...message_ids]; };
+const recycleStaleMessages = () => {
+	// if there are stale assigned messages dequeue, unassign, and re-enqueue them
+	const stale_ids = dequeueStaleMessageIds();
+	stale_ids.forEach(markAsUnassigned);
+	enqueueMessageIds(...stale_ids);
+};
+const removeMessageId = (message_id) => { message_id_queue = _.without(message_id_queue, message_id); };
 const dequeueStaleMessageIds = () => {
 	let has_checked_for_stale_ids = false;
 	let current_index = 0;
@@ -34,6 +38,8 @@ const dequeueStaleMessageIds = () => {
 	message_id_queue = message_id_queue.slice(current_index);
 	return stale_ids;
 };
+
+// ** Queue Accessors **
 
 const allUnassigned = () => {
 	let has_checked_for_unassigned_ids = false;
@@ -51,43 +57,41 @@ const allUnassigned = () => {
 	return unassigned_ids;
 };
 
-const recycleStaleMessages = () => {
-	// if there are stale assigned messages dequeue, unassign, and re-enqueue them
-	const stale_ids = dequeueStaleMessageIds();
-	stale_ids.forEach(markAsUnassigned);
-	enqueueMessageIds(...stale_ids);
-};
 
 /**************************************
 * Database Abstraction
 ***************************************/
 
 // this is object will act as our DB
-// a map keyed by, with message data/metadata value
+// a map keyed by id, with message data/metadata value
 let message_db = {};
+
+// ** DB Mutators **
 
 const createAndSaveMessage = ({message_text}) => {
 	const message = {
 		id: uuid(),
-		text,
+		message_text,
 		created_at: moment(),
 		assigned_at: null
 	};
 	saveMessage(message);
 	return message;
 }
-
 const saveMessage = (message) => { message_db = {...message_db, [message.id]: message}; }
-const getMessageById = (message_id) => message_db[message_id];
 const markAsAssigned = (message_id) => { message_db[message_id].assigned_at = moment(); };
 const markAsUnassigned = (message_id) => { message_db[message_id].assigned_at = null; };
+const deleteMessage = (message_id) => { message_db = _.omit(message_id); };
+
+// ** DB Accessors **
+
+const getMessageById = (message_id) => message_db[message_id];
 const isMessageUnassigned = (message_id) => _.isEmpty(message_db[message_id].assigned_at);
 const isMessageStale = (message_id) => {
 	const message = message_db[message_id];
 	// TODO: use moment helpers
-	return message.assigned_at && ((moment() - message.assigned_at) < process.env.time_limit)
+	return message.assigned_at && ((moment() - message.assigned_at) < process.env.TIME_LIMIT_IN_MINUTES)
 };
-const deleteMessage = (message_id) => { message_db = _.omit(message_id); };
 
 
 /**************************************
@@ -99,12 +103,12 @@ app.post('/message', (req, res) => {
 	// TODO: use POST body
 	// message_text = _.get(req, 'body.name');
 	const message_text = _.get(req, 'query.text');
-	if (message_text) {
+	if (message_text){
 		const message = createAndSaveMessage({message_text});
 		enqueueMessageIds(message.id)
 		res.send(200, `Your message was registered with id: ${message.id}`);
 	} else {
-		res.send(404, 'Message creation failed, try adding text');
+		res.send(401, 'Message creation failed, try adding text');
 	}
 });
 
@@ -118,12 +122,18 @@ app.get('/message', (req, res) => {
 
 // consumer processes message (Delete)
 app.delete('/message', (req, res) => {
-	console.log(req);
-  res.send('DELETE request to process message')
+	const message_id = _.get(req, 'query.message_id');
+	if (message_id){
+		deleteMessage(message_id);
+		res.send(200, `Message with id: ${message_id} marked as processed`);
+	} else {
+		res.send(404, 'Message with that id does not exist');
+	}
 });
 
 // debug endpoint
 app.get('/state', (req, res) => {
+	// TODO: remove logging
 	console.log('queue', message_id_queue);
 	console.log('db', message_db);
 	res.send(_.map(message_id_queue, id => (message_db[id])))
